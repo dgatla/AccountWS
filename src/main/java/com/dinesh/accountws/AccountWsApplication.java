35611797782db7ef4c84bcbcce58a0fa6e93baff
dgatla <dinessh.ga@outlook.com>
2024-10-20 15:42:50 +0530

    initial commit

---
+++ src/main/java/com/dinesh/accountws/AccountWsApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.accountws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountWsApplication.class, args);
+    }
+
+}
