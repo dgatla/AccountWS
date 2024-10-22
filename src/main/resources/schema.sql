222dc45bc049d0074434520c19408f35fb4860d5
dgatla <dinessh.ga@outlook.com>
2024-10-22 06:30:51 +0530

    change create_dt column name to created_at

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `customers`(
     `name` varchar(100) NOT NULL,
     `email` varchar(100) NOT NULL,
     `mobile_number` varchar(10) NOT NULL,
-    `create_dt` date not NULL,
+    `created_at` timestamp not NULL,
     `created_by` varchar(20) NOT NULL,
     `updated_at` timestamp NOT NULL,
     `updated_by` varchar(20) NOT NULL
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS `accounts`(
      `account_id` int AUTO_INCREMENT PRIMARY KEY,
      `account_type` varchar(20) NOT NULL,
      `branch_address` varchar(100) NOT NULL,
-     `create_dt` timestamp not NULL,
+     `created_at` timestamp not NULL,
      `created_by` varchar(20) NOT NULL,
      `updated_at` timestamp NOT NULL,
      `updated_by` varchar(20) NOT NULL
