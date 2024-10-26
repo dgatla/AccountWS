cf2ee5539f98b542e40aca4949897e7861bb4e2f
dgatla <dinessh.ga@outlook.com>
2024-10-26 21:53:23 +0530

    add-repositories

---
+++ src/main/java/com/dinesh/accountws/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.dinesh.accountws.repository;
+
+import com.dinesh.accountws.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
